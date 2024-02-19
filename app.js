ca8571676fc0e6268f1dfd0b0aee9b6e4236356d
jovid_18 <cshcho99@gmail.com>
2024-02-19 20:39:37 +0900

    update redis

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
-const passport = require('passport');
-const helmet = require('helmet');
-const hpp = require('hpp');
-const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const redis = require("redis");
+const RedisStore = require("connect-redis").default;
 
 dotenv.config();
 const redisClient = redis.createClient({
@@ -18,35 +18,35 @@ const redisClient = redis.createClient({
   legacyMode: true,
 });
 redisClient.connect().catch(console.error);
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const postRouter = require('./routes/post');
-const userRouter = require('./routes/user');
-const { sequelize } = require('./models');
-const passportConfig = require('./passport');
-const logger = require('./logger');
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
+const logger = require("./logger");
 
 const app = express();
 passportConfig();
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'html');
-nunjucks.configure('views', {
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
   express: app,
   watch: true,
 });
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log('데이터베이스 연결 성공');
+    console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
   });
 
 //배포 환경 판단
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //더 많은 사용자 정보를 로그로 남김
-  app.use(morgan('combined'));
+  app.use(morgan("combined"));
   app.use(
     helmet({
       contentSecurityPolicy: false,
@@ -56,10 +56,10 @@ if (process.env.NODE_ENV === 'production') {
   );
   app.use(hpp());
 } else {
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/img", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -73,7 +73,7 @@ const sessionOption = {
   },
   store: new RedisStore({ client: redisClient }),
 };
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   sessionOption.proxy = true;
   //sessionOption.cookie.secure = true;
 }
@@ -81,15 +81,15 @@ app.use(session(sessionOption));
 app.use(passport.initialize()); // 요창(req 객체)에 passport 설정을 심고
 app.use(passport.session()); // req.session 객체에 passport 정보를 저장
 
-app.use('/', pageRouter);
-app.use('/auth', authRouter);
-app.use('/post', postRouter);
-app.use('/user', userRouter);
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
+app.use("/post", postRouter);
+app.use("/user", userRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
   error.status = 404;
-  logger.info('hello');
+  logger.info("hello");
   logger.error(error.message);
   next(error);
 });
@@ -97,9 +97,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   console.error(err);
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
